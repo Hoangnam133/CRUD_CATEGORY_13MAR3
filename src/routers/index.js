b80affe1a599cb302365d91a51248093d88d32d2
Hoangnam133 <012003hoangnam@gmail.com>
2025-03-13 10:59:00 +0700

    crud-update

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,4 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 router.use('/product',require('./product/index'))
+router.use('/category',require('./category/index'))
+
 module.exports = router
