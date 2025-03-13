0e2f605441307539afcb367dc0ba009b6a6af251
Hoangnam133 <012003hoangnam@gmail.com>
2025-03-13 10:59:56 +0700

    crud-update1

--- src/app.js
+++ src/app.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 require('./configs/connectDB')
-app.use('/',require('./routers/index'))
+app.use('/api/v1',require('./routers/index'))
 module.exports = app
